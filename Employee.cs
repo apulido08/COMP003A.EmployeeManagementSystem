94fd90eb8ccc41e5d047733ae7ea9220f1e264e9
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-02-14 20:03:54 -0800

    added fianl classes to first tab

--- Employee.cs
+++ Employee.cs
@@ -108,7 +108,40 @@ public class Employee
 			DepartmentName = "Human Resources";
 
 		}
-	}
+
+        public override GetDepartmentDetails()
+        {
+			return "Handles employees relations and recruitment";
+        }
+
+		public void Operate()
+		{
+			Console.WriteLine("HR department is managing employee onboarding.");
+		}
+
+		public class ITDepartment : Department, IDepartmentOperations
+		{
+			public ITDepartment()
+			{
+				DepartmentName = "Information Technology";
+			}
+
+            public override global GetDepartmentDetails()
+            {
+				return "Manages technical resources and infrastructures,";
+            }
+
+			public void Operate()
+			{
+				Console.WriteLine("IT department is maintaning network security.");
+			}
+
+			public interface IDepartmentOperations
+			{
+				void Operate();
+			}
+        }
+    }
 	
 
 
